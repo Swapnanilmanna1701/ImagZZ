f33534aafce5bf4c395891ac4a43f59e9c787730
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-07-07 00:14:52 +0530

    authentication setup

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+ 
+export default authMiddleware({
+  
+});
+ 
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
