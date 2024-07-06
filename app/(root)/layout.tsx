833cad384ffa9854961b11715e4363203afe6255
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-07-07 02:09:41 +0530

    work in progress

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import MobileNav from '@/components/shared/MobileNav';
 import Sidebar from '@/components/shared/Sidebar';
 import React from 'react'
 
@@ -5,6 +6,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <Sidebar />
+      <MobileNav />
     
 
     <div className="root-container">
