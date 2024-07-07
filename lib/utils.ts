053f7c200f752901d42cd5cf3476af6501f657d4
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-07-07 13:23:19 +0530

    issue solved

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,8 @@
+
 /* eslint-disable prefer-const */
 /* eslint-disable no-prototype-builtins */
 import { type ClassValue, clsx } from "clsx";
-import qs from "qs";
+import qs from 'qs';
 import { twMerge } from "tailwind-merge";
 
 import { aspectRatioOptions } from "@/constants";
